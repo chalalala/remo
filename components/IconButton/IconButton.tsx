8c233cdeae9115c7ee4087051b24a61d9e8f3105
Mai Doan <doantuyetmai2000@gmail.com>
2024-04-01 23:04:55 +0700

    Fix warning console (#38)

--- components/IconButton/IconButton.tsx
+++ components/IconButton/IconButton.tsx
@@ -1,19 +1,22 @@
 import clsx from 'clsx';
-import React, { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react';
+import React, { ButtonHTMLAttributes, FC, PropsWithChildren, forwardRef } from 'react';
 
 type Props = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
-export const IconButton: FC<Props> = ({ children, className, ...buttonProps }) => {
-  return (
-    <button
-      className={clsx(
-        'aspect-square rounded-full p-1 transition-all',
-        'hover:bg-indigo-50 hover:text-indigo-500',
-        className,
-      )}
-      {...buttonProps}
-    >
-      {children}
-    </button>
-  );
-};
+export const IconButton: FC<Props> = forwardRef<HTMLButtonElement, Props>(
+  ({ children, className, ...buttonProps }, ref) => {
+    return (
+      <button
+        className={clsx(
+          'aspect-square rounded-full p-1 transition-all',
+          'hover:bg-indigo-50 hover:text-indigo-500',
+          className,
+        )}
+        ref={ref}
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    );
+  },
+);
